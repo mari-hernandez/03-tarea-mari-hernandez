88f1954332caad5bffbb991ced2a6019413d9e3d
mari-hernandez <70488051+mari-hernandez@users.noreply.github.com>
2020-11-08 13:00:46 -0300

    implementa rk4

--- codigos/planeta.py
+++ codigos/planeta.py
@@ -1,6 +1,16 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+G = 1
+M = 1
+m = 1
+
 class Planeta(object):
     """
-    Complete el docstring.
+    La clase planeta, crea un planeta dadas su condiciones iniciales de posicion y velocidad
+    ademas de un alpha opcional.Posee metodos para la ecuacion de
+    movimiento de este, y los metodos de runge Kutta 4, Verlet y Beeman que
+    avanzan un paso. Tambien se puede calcular la energía total del planeta en las condiciones actuales
     """
 
     def __init__(self, condicion_inicial, alpha=0):
@@ -23,18 +33,31 @@ class Planeta(object):
         primer orden.
         """
         x, y, vx, vy = self.y_actual
-        # fx = ...
-        # fy = ...
+        r = (x**2 + y**2)**(1/2)
+        fx =  - x * (G * M * m / r**3 - 2 * self.alpha * G * M * m / r**4)
+        fy =  - y * (G * M * m / r**3 - 2 * self.alpha * G * M * m / r**4)
         return [vx, vy, fx, fy]
 
     def avanza_rk4(self, dt):
-        """
-        Toma la condición actual del planeta y avanza su posicion y velocidad
-        en un intervalo de tiempo dt usando el método de RK4. El método no
-        retorna nada, pero modifica los valores de self.y_actual y de
-        self.t_actual.
-        """
-        pass
+        x, y, vx, vy=self.y_actual                         
+        k1=self.ecuacion_de_movimiento()              
+
+        self.y_actual=[x+k1[0]/2, y+k1[1]/2, vx+k1[2]/2, vy+k1[3]/2]
+        k2=self.ecuacion_de_movimiento()
+        
+        self.y_actual=[x+k2[0]/2, y+k2[1]/2, vx+k2[2]/2, vy+k2[3]/2]
+        k3=self.ecuacion_de_movimiento()
+
+        self.y_actual=[x+k3[0], y+k3[1], vx+k3[2], vy+k3[3]]
+        k4=self.ecuacion_de_movimiento()
+
+        x_n=x+(1/6)*dt*(k1[0]+2*k2[0]+2*k3[0]+k4[0])         
+        y_n=y+(1/6)*dt*(k1[1]+2*k2[1]+2*k3[1]+k4[1])        
+        vx_n=vx+(1/6)*dt*(k1[2]+2*k2[2]+2*k3[2]+k4[2])    
+        vy_n=vy+(1/6)*dt*(k1[3]+2*k2[3]+2*k3[3]+k4[3])    
+        self.y_actual=[x_n, y_n, vx_n, vy_n]
+        self.t_actual+=dt
+        pass 
 
     def avanza_verlet(self, dt):
         """
@@ -49,9 +72,15 @@ class Planeta(object):
         pass
 
     def energia_total(self):
-        """
+        '''
         Calcula la enérgía total del sistema en las condiciones actuales.
-        """
-        pass
+        '''
+        x, y, vx, vy = self.y_actual
+        r=(x**2+y**2)**(1/2)
+        energia_Cinetica = (1/2)*m*(vx**2+vy**2)
+        energia_Potencial = -G*M*m/r + self.alpha*G*M*m/r**2
+        return energia_Cinetica + energia_Potencial
 
+
+ 
 
--- codigos/solucion_usando_planeta.py
+++ codigos/solucion_usando_planeta.py
@@ -1,10 +1,38 @@
 from planeta import Planeta
+import numpy as np
+import matplotlib.pyplot as plt
 
-condicion_inicial = [10, 0, 0, XXX]
+#Condiciones iniciales
+condicion_inicial=[10, 0, 0, 0.26]  
+mercurio=Planeta(condicion_inicial)
+pasos=800                            
+dt=800/pasos                          
+T=np.linspace(0,800, pasos)   
+x=[10]
+y=[0]
+energia=[mercurio.energia_total()]
 
-mercurio = Planeta(condicion_inicial)
+N=1        
+while N<pasos:               
+    mercurio.avanza_rk4(dt)
+    N+=1
+    x.append(mercurio.y_actual[0])
+    y.append(mercurio.y_actual[1])
+    energia.append(mercurio.energia_total())
+#Plots
+fig=plt.figure(1)
+fig.clf()
+plt.plot(x,y)
+plt.title('Trayectoria vs Tiempo \n RK4 con v_{y}(0)=0.26$')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
 
+fig2=plt.figure(2)
+fig2.clf()
+plt.plot(T, energia)
+plt.title('Energia vs Tiempo \n RK4 con v_{y}(0)=0.26')
+plt.xlabel('Tiempo')
+plt.ylabel('Energia')
+plt.show()
 
-"""
-Complete el código a continuación.
-"""
